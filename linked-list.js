37d884e53d949d2f64b6843357d31601679404bc
Tuan Ha <hatuan@email.cz>
2025-12-01 20:27:43 +0100

    Create append function to add a node to the list
    The append function adds a note to the end of
    the list. First it checks whether the list is empty
    -> if yeas then the head's next becomes the node.
    
    Else it iterates over all the nodes untill it
    finds the tail (the tail will have next === null).
    
    Also added some checking for when our tmp is not
    a object or if is null or if the tmp's next is
    actually not null
    - this should not even happen but still.

--- linked-list.js
+++ linked-list.js
@@ -7,8 +7,27 @@ class LinkedList {
     };
   }
 
-  get head() { return this.#head };
-  set head(node) { this.#head = node };
+  append(node) {
+    if (this.head.next === null) {
+      this.head.next = node;
+      return;
+    }
+
+    let tmp = this.head.next;
+    while (tmp.next != null) {
+      tmp = tmp.next;
+    }
+    if (tmp.next != null || tmp === null || typeof tmp != "object") return;
+    
+    tmp.next = node;
+  }
+
+  get head() {
+    return this.#head;
+  }
+  set head(node) {
+    this.#head = node;
+  }
 }
 
 export { LinkedList };
